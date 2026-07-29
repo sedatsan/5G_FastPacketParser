c5b3bd3568bf7cd0ccd0c45fd3b88d63e1fd05a8
Sedat SANCAKLI <sedat.sancakli@student.put.poznan.pl>
2026-07-29 03:08:11 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+#include <iomanip>
 #include <iostream>
 
-int main(){
+#include "../include/MemoryMappedFile.hpp"
+#include "../include/PcapStructs.hpp"
 
-    std::cout<<"Hello World";
+int main() {
+    MemoryMappedFile my_file("../gnb_mac.pcap");
+    auto memory_span = my_file.file_reader();
+
+    if (memory_span.size() < sizeof(PcapGlobalHeader)) {
+        throw std::runtime_error("File is too small.");
+    }
+
+    const PcapGlobalHeader* header = reinterpret_cast<const PcapGlobalHeader*>(memory_span.data());
+
+    std::cout << "Magic Number: 0x" << std::hex << header->magic_number << std::endl;
 
     return 0;
 }
