0e8f7e6b0ce19265a96ebf2a97dcb7e78119d21d
Sedat SANCAKLI <sedat.sancakli@student.put.poznan.pl>
2026-07-29 22:55:50 +0200

    tested packet count functionality on main

--- src/main.cpp
+++ src/main.cpp
@@ -16,5 +16,18 @@ int main() {
 
     std::cout << "Magic Number: 0x" << std::hex << header->magic_number << std::endl;
 
+    size_t current_offset = sizeof(PcapGlobalHeader);
+
+    int packet_count = 0;
+
+    while (current_offset < memory_span.size()) {
+        const PcapRecordHeader* record_header =
+            reinterpret_cast<const PcapRecordHeader*>(memory_span.data() + current_offset);
+        current_offset += sizeof(PcapRecordHeader) + record_header->captured_len;
+        packet_count++;
+    }
+
+    std::cout << "Packet Count: " << packet_count << std::endl;
+
     return 0;
 }
