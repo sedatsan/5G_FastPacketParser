b47940b4ae9e68faca3424a61a68fd3443acaf4f
Sedat SANCAKLI <sedat.sancakli@student.put.poznan.pl>
2026-07-29 22:54:52 +0200

    feat: implemented Pcap packet record header

--- include/PcapStructs.hpp
+++ include/PcapStructs.hpp
@@ -8,3 +8,10 @@ struct __attribute__((__packed__)) PcapGlobalHeader {
     uint32_t snap_len;
     uint32_t link_type;
 };
+
+struct __attribute__((__packed__)) PcapRecordHeader {
+    uint32_t timestamp_sec;
+    uint32_t timestamp_usec;
+    uint32_t captured_len;
+    uint32_t origin_len;
+};
